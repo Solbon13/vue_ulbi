d50082c95a5b3218f93efd0d39f242ec0a69db02
Solbon13 <solbon13@yandex.ru>
2021-05-05 09:59:41 +0800

    first vue

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import router from '@/router'
+import axios from 'axios'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false
+Vue.prototype.axios = axios
+
+new Vue({
+  vuetify,
+  router,
+  render: h => h(App)
+}).$mount('#app')
